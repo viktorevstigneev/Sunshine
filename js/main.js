6b4448f2dcd88d46f134d35a5bb4afedc9c82a7a
viktorevstigneev <viktor_evstigneev14@bk.ru>
2020-03-24 00:49:00 +0300

    create a project

---
+++ js/main.js
@@ -0,0 +1,16 @@
+
+
+ $(function(){
+ var a = new String;
+ a = $('.text_cont_inner').text();
+ $('.text_cont_inner').text('');
+ var c=a.length;
+ j=0;
+ setInterval(function(){
+ if(j<c){
+ $('.text_cont_inner').text($('.text_cont_inner').text()+a[j]);
+ j=j+1;
+ }
+ else {$('.text_cont_inner').removeClass('after')}
+ },400);
+ });
